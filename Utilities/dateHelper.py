3d09db88c158547a06f97485ce5bde7f821b8e52
Vickesh Kant <vickesh.kant@gmail.com>
2021-10-22 14:43:34 -0400

    rolling 13 month list added

--- Utilities/dateHelper.py
+++ Utilities/dateHelper.py
@@ -14,10 +14,14 @@ def date_helper(date, return_value):
     matching_row = df1.loc[df1['Date'] == date]
     if return_value == 'fiscal_yr':
         return (matching_row['Fiscal Year'].values)
+    elif return_value == 'year':
+        return (matching_row['Year'].values)
     elif return_value == 'fiscal_qtr':
         return (matching_row['Quarter'].values)
     elif return_value == 'month_num':
         return (matching_row['Month No.'].values)
+    elif return_value == 'month_id':
+        return (matching_row['Month ID'].values)
     elif return_value == 'month_nm':
         return (matching_row['Month Nm.'].values)
     elif return_value == 'period_num':
@@ -126,6 +130,43 @@ def rolling_13_period_list(date):
         list_current_yr.append(period_id)
         
     for i in range(13, (13 - periods_from_prior_yr), -1):
+        period_id = str(prior_year) + str(i).zfill(2)
+        list_prior_yr.append(period_id)
+    
+    # returning list of YTD periods minus the current period currently in plus all periods prior year
+    return tuple(list_prior_yr + list_current_yr)
+
+def rolling_13_month_list(date):
+    date  = np.datetime64(date)
+    
+    # list variables to hold period ID's
+    list_current_yr = []
+    list_prior_yr = []
+
+    # calling date helper to find out what the fiscal period number and fiscal year is based on today's date
+    current_period = date_helper(date, 'month_id')
+    current_year = date_helper(date, 'year')
+    current_year = int(current_year)
+    prior_year = current_year - 1
+
+    # finding the prior period based on today's date
+    prior_period = int(current_period) - 1
+    
+    # condition to make sure prior period is 13 if current period is period 1
+    if prior_period == 0:
+        prior_period = 12
+        current_year = current_year - 1
+        prior_year = prior_year - 1
+
+    # find out number of periods from prior year
+    periods_from_prior_yr = 12 - prior_period
+
+    # for loop to create list of periods up to current date - excluding current period
+    for i in range(prior_period):
+        period_id = str(current_year) + str(i+1).zfill(2)
+        list_current_yr.append(period_id)
+        
+    for i in range(12, (12 - periods_from_prior_yr), -1):
         period_id = str(prior_year) + str(i).zfill(2)
         list_prior_yr.append(period_id)
     
